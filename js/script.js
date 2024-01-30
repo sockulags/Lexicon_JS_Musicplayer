de81cc33c9aef2927df23edeab380db75f0ec309
Lucas Skog <lucasskog@gmail.com>
2024-01-30 10:42:42 +0100

    Play buttons work everywhere now, and is connected

--- js/script.js
+++ js/script.js
@@ -1,41 +1,81 @@
 import { buttons } from "./constants.js";
 import { songs } from "./songs.js";
 
+const mediaPlayer = document.querySelector(".mediaplayer");
+
+const coverImg = mediaPlayer.querySelector(".img-container img");
+const title = mediaPlayer.querySelector(".song-name");
+const artist = mediaPlayer.querySelector(".artist");
+
+const audioPlayer = document.querySelector('audio');
 const songList = document.querySelector(".songs");
-const songContainers = document.querySelectorAll(".song-container");
+const switchViewButton = document.querySelector(".tab");
+const controls = document.querySelector(".control-btns");
+const loopBtn = controls.querySelector(".loop-btn");
+const shuffleBtn  = controls.querySelector(".shuffle-btn");
+const playButton = controls.querySelector(".mediaplayer .play-btn");
+const nextSong = controls.querySelector(".next-btn");
+const previousSong = controls.querySelector(".prev-btn");
 
 let currPlayBtn = null;
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', initialize);
+
+function initialize(){
+    populateSongList();
+    switchViewButton.addEventListener("click", switchView)
+    songList.addEventListener('click', handleSongClick)
+    updateMediaPlayerSong(songs[0]);
+    playButton.addEventListener('click', togglePlay);
+}
+
+function togglePlay(){
+const songId = mediaPlayer.getAttribute("song-id");
+const songContainer = document.querySelector(`[song-id="${songId}"]`);
+
+const playBtn = songContainer.querySelector('.play-btn');
+handlePlayButtonClick(playButton.textContent === buttons.play, songContainer, playBtn)
+}
+
+function handlePlayButtonClick(isPlaying, container, button){
+    playSong(container, !isPlaying);
+    button.textContent =  isPlaying ? buttons.pause : buttons.play;
+    playButton.textContent =  isPlaying ? buttons.pause : buttons.play;
+}
+
+function populateSongList(){
     songs.forEach(song => {
         songList.innerHTML += songContainer(song);
     });
-    songList.addEventListener('click', (e) => {
-        const clickedContainer = e.target.closest('.song-container');
-        if (clickedContainer) {
-            const playBtn = clickedContainer.querySelector('.play-btn');
+}
 
-            // If the current play button is not the clicked one and is in 'pause' state
-            if (currPlayBtn && currPlayBtn !== playBtn && currPlayBtn.textContent === buttons.pause) {
-                currPlayBtn.textContent = buttons.play;
-            }
+function switchView(){
+    songList.classList.toggle("hider");
+}
+function updateMediaPlayerSong(song) {
+    mediaPlayer.setAttribute("song-id", song.id)
+    coverImg.setAttribute("src", song.cover);
+    title.textContent = song.title;
+    artist.textContent = song.artist;
+}
 
-            if (playBtn && playBtn.textContent === buttons.play) {
-                playSong(clickedContainer, false);
-                playBtn.textContent = buttons.pause;
-            } else if (playBtn) {
-                playSong(clickedContainer, true);
-                playBtn.textContent = buttons.play;
-            }
+function handleSongClick(e) {
+    const clickedContainer = e.target.closest('.song-container');
+    if (!clickedContainer) return;
 
-            currPlayBtn = playBtn; // Update the current play button
-        }
-    });
-});
+    const playBtn = clickedContainer.querySelector('.play-btn');
+    if (!playBtn) return;
 
+    if (currPlayBtn && currPlayBtn !== playBtn && currPlayBtn.textContent === buttons.pause) {
+        currPlayBtn.textContent = buttons.play;
+    }
+    handlePlayButtonClick(playBtn.textContent === buttons.play, clickedContainer, playBtn)
+
+    currPlayBtn = playBtn;
+}
 
 function songContainer(song){
-   return `<div class="song-container">
+   return `<div class="song-container" song-id="${song.id}">
     <div class="song-info">
       <h2 class="song-name">${song.title}</h2>
       <h3 class="artist">${song.artist}</h3>
@@ -53,16 +93,14 @@ let currentSongTime = 0;
 
 function playSong(clickedContainer, isPlaying) {   
     const songTitle = clickedContainer.querySelector('.song-name').textContent;
-    const song = songs.find(s => s.title === songTitle);
+    const song = songs.find(s => s.title === songTitle);   
 
-    const audioPlayer = document.querySelector('audio');
-
-    if (song) {
-        // Check if the clicked song is different from the current song
+    if (song) {   
         if (currentSong !== song.src) {
             currentSong = song.src;
             audioPlayer.src = song.src;
-            currentSongTime = 0;
+            currentSongTime = 0;    
+            updateMediaPlayerSong(song)        
         }
         if (isPlaying) {          
             currentSongTime = audioPlayer.currentTime;
--- js/songs.js
+++ js/songs.js
@@ -5,36 +5,42 @@ const JPG = ".jpg";
 
 export const songs = [
   {
+    id: 1,
     artist: "Avicii",
     title: "Wake me up",
     src: AUDIO + "Avicii_Wake_Me_Up" + MP3,
     cover: IMG + "avicii_wakemeup_image" + JPG,
   },
   {
+    id: 2,
     artist: "Coldplay",
     title: "Viva la vida",
     src: AUDIO + "Coldplay_Viva_La_Vida" + MP3,
     cover: IMG + "coldplay_vivalavida" + JPG,
   },
   {
+    id: 3,
     artist: "Iron Maiden",
     title: "The number of the beast",
     src: AUDIO + "Iron_Maiden_The_Number_Of_The_Beast" + MP3,
     cover: IMG + "ironmaiden_nrofthebeast" + JPG,
   },
   {
+    id: 4,
     artist: "Metallica",
     title: "Enter Sandman",
     src: AUDIO + "Metallica_Enter_Sandman" + MP3,
     cover: IMG + "enter_sandman" + JPG,
   },
   {
+    id: 5,
     artist: "Korn",
     title: "Coming undone",
     src: AUDIO + "Korn_Coming_Undone" + MP3,
     cover: IMG + "korn_comingundone" + JPG,
   },
   {
+    id: 6,
     artist: "Swedish House Mafia ft. John Martin",
     title: "Don't you worry child",
     src:
@@ -42,12 +48,14 @@ export const songs = [
     cover: IMG + "shm_dontyouworrychild" + JPG,
   },
   {
+    id: 7,
     artist: "Tom Odell",
     title: "Another love",
     src: AUDIO + "Tom_Odell_Another_Love" + MP3,
     cover: IMG + "tomodell_anotherlove" + JPG,
   },
   {
+    id: 8,
     artist: "System Of A Down",
     title: "Chop suey",
     src: AUDIO + "System_Of_A_Down_Chop_Suey" + MP3,
